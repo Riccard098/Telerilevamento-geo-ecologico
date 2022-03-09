86ab7d77c61d4ce98aac3502bf1406fa00037e11
Riccard098 <101262928+Riccard098@users.noreply.github.com>
2022-03-09 13:41:06 +0100

    Create Myfirstscript.R

---
+++ Myfirstscript.R
@@ -0,0 +1,18 @@
+
+h_veg<-c("10", "7", "5", "3", "1")
+
+
+
+luce<-c("8", "3", "3", "2", "1")
+
+
+
+
+
+h_veg
+piombo
+
+
+
+
+plot(x=h_veg, y=luce, main="vegetazione vs piombo", pch=4, cex=0.5, xlab="altezza vegetazione" )
