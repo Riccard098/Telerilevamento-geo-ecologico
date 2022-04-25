95b0b5d31786f005be4e7941de2f719b53551e83
Riccard098 <101262928+Riccard098@users.noreply.github.com>
2022-04-25 09:29:08 +0200

    Update R:_code_land_cover.r

--- R:_code_land_cover.r
+++ R:_code_land_cover.r
@@ -1,2 +1,17 @@
 library(raster)
 library(RStoolbox)
+setwd("C:/Users/kirir/OneDrive/Desktop/lab")
+library(ggplot2)
+ggRGB(l92, r=1, g=2, b=3, stretch="lin")
+ggRGB(l06, r=1, g=2, b=3, stretch="lin")
+library(patchwork)
+p1 <- ggRGB(l92, r=1, g=2, b=3, stretch="lin")
+p2 <- ggRGB(l06, r=1, g=2, b=3, stretch="lin")
+p1+p2
+p1/p2
+l92c <- unsuperClass(l92, nClasses=2)
+l06c <- unsuperClass(l06, nClasses=2)
+plot(l92c$map)
+plot(l06c$map)
+freq(l92c$map)
+freq(l06c$map)
