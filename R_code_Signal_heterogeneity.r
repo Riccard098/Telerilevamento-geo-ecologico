ab697a3acab2f349611fb509238471dccb60d892
Riccard098 <101262928+Riccard098@users.noreply.github.com>
2022-05-20 11:32:17 +0200

    Create R_code_Signal_heterogeneity.r

---
+++ R_code_Signal_heterogeneity.r
@@ -0,0 +1,34 @@
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(patchwork)
+library(viridis)
+setwd("C:/Users/kirir/OneDrive/Desktop/lab")
+sen <- brick("sentinel.png")
+ggRGB(sen, 1, 2, 3, stretch="hist")
+ggRGB(sen, 2, 1, 3, stretch="hist")
+sen_pca <- rasterPCA(sen)
+summary(sen_pca$model)
+plot(sen_pca$map)
+pc1 <- sen_pca$map$PC1
+pc2 <- sen_pca$map$PC2
+pc3 <- sen_pca$map$PC3
+g1 <- ggplot() + geom_raster(pc1, mapping=aes(x=x, y=y, fill=PC1))
+g2 <- ggplot() + geom_raster(pc2, mapping=aes(x=x, y=y, fill=PC2))
+g3 <- ggplot() + geom_raster(pc3, mapping=aes(x=x, y=y, fill=PC3))
+g1+g2+g3
+sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
+ggplot() + geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer))
+ggplot() + geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno")
+im1 <- ggRGB(sen, 2, 1, 3)
+im2 <- ggplot() + geom_raster(pc1, mapping=aes(x=x, y=y, fill=PC1))
+im3 <- ggplot() + geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno") 
+im1 + im2 + im3
+sd5 <- focal(pc1, matrix(1/25, 5, 5), fun=sd)
+ggplot() + geom_raster(sd5, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno")
+im4 <- ggplot() + geom_raster(sd5, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno")
+im3 + im4
+sd7 <- focal(pc1, matrix(1/49, 7, 7), fun=sd)
+ggplot() + geom_raster(sd7, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno")
+im5 <- ggplot() + geom_raster(sd7, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno")
+im1 + im2 + im3 + im4 + im5
