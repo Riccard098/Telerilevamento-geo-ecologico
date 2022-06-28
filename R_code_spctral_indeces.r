32335b56bfa76f6c4a6bc11df1155dc9684fecd3
Riccard098 <101262928+Riccard098@users.noreply.github.com>
2022-06-28 11:32:56 +0200

    Update R_code_spctral_indeces.r

--- R_code_spctral_indeces.r
+++ R_code_spctral_indeces.r
@@ -1,42 +1,42 @@
-#recall library
+# Recall library
 library(raster)
 library(RStoolbox)
 library(rasterdiv)
 
-#import datas
+# Set datawork
 setwd("C:/Users/kirir/OneDrive/Desktop/lab")
 
-#rename datas
+# Import and rename datas
 l1992 <- brick("defor1_.jpg")
 l2006 <- brick("defor2_.jpg")
 
-#Difference Vegetation Index (DVI) of 1992's datas
+# Difference Vegetation Index (DVI) of 1992's datas
 dvi1992 = l1992[[1]] - l1992[[2]]
 
-#Calculete Normalized Difference Vegetation Index (NDVI) of 1992's datas
+# Calculete Normalized Difference Vegetation Index (NDVI) of 1992's datas
 ndvi1992 = (l1992[[1]] - l1992[[2]]) / (l1992[[1]] + l1992[[2]])
 
-#Create a specific palette
+# Create a specific palette
 cl <- colorRampPalette(c("dark blue", "yellow", "red","black")) (100)
 
-#Plot in the same window the RGB plot of 1992 datas and the NDVI of 1992's
+# Plot in the same window the RGB plot of 1992 datas and the NDVI of 1992's
 par(mfrow=c(2,1))
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 plot(ndvi1992, col=cl)
 
-#Calculete Difference Vegetation Index (DVI) of 2006's datas
+# Calculete Difference Vegetation Index (DVI) of 2006's datas
 dvi2006 = l2006[[1]] - l2006[[2]]
 
-#Calculete Normalized Difference Vegetation Index (NDVI) of 2006's datas
+# Calculete Normalized Difference Vegetation Index (NDVI) of 2006's datas
 ndvil2006 = (l2006[[1]] - l2006[[2]]) / (l2006[[1]] + l2006[[2]])
 
-#Plot in the same window the NDVI of 1992's datas and the NDVI of 2006's
+# Plot in the same window the NDVI of 1992's datas and the NDVI of 2006's
 par(mfrow=c(2,1))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
 
-#Calculate all the Spectral Indecis for the 1992's datas
+# Calculate all the Spectral Indecis for the 1992's datas
 si1992 <- spectralIndices(l1992, green=3, red=2, nir=1)
 
-#Plot all the Spectral Indecis for the 1992's datas
+# Plot all the Spectral Indecis for the 1992's datas
 plot(si1992, col = cl)
