19ed49710bae20e1be8516ca6b666f762f21d135
Riccard098 <101262928+Riccard098@users.noreply.github.com>
2022-07-04 11:32:13 +0200

    Update and rename R_code_Signal_heterogeneity.r to R_code_signal_heterogeneity.r

--- R_code_signal_heterogeneity.r
+++ R_code_signal_heterogeneity.r
@@ -1,22 +1,42 @@
+# Recall libraries
 library(raster)
 library(RStoolbox)
 library(ggplot2)
 library(patchwork)
 library(viridis)
+
+# Set Workdirectory
 setwd("C:/Users/kirir/OneDrive/Desktop/lab")
+
+# Import and rename datas
 sen <- brick("sentinel.png")
+
+# RGBplot datas in different ways
 ggRGB(sen, 1, 2, 3, stretch="hist")
 ggRGB(sen, 2, 1, 3, stretch="hist")
+
+# Import a single data from a pack
 sen_pca <- rasterPCA(sen)
 summary(sen_pca$model)
+
+# Plot the data
 plot(sen_pca$map)
+
+# Import and rename datas
 pc1 <- sen_pca$map$PC1
 pc2 <- sen_pca$map$PC2
 pc3 <- sen_pca$map$PC3
+
+
+# Rename the plots
 g1 <- ggplot() + geom_raster(pc1, mapping=aes(x=x, y=y, fill=PC1))
 g2 <- ggplot() + geom_raster(pc2, mapping=aes(x=x, y=y, fill=PC2))
 g3 <- ggplot() + geom_raster(pc3, mapping=aes(x=x, y=y, fill=PC3))
+
+# Patch plots toghether
 g1+g2+g3
+
+#
 sd3 <- focal(pc1, matrix(1/9, 3, 3), fun=sd)
 ggplot() + geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer))
 ggplot() + geom_raster(sd3, mapping=aes(x=x, y=y, fill=layer)) + scale_fill_viridis(option="inferno")
