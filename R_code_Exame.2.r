5cac62e54fc0108513404f96768f23c69976733d
Riccardo Biagini <101262928+Riccard098@users.noreply.github.com>
2022-07-16 11:10:32 +0200

    Update R_code_Exame.2.r

--- R_code_Exame.2.r
+++ R_code_Exame.2.r
@@ -136,27 +136,14 @@ perc_frost_20
 perc_frost_22
 
 
-#create a dataframe
+# Create a dataframe
 Years <- c("2000", "2005", "2010", "2015", "2020", "2022")
 Percent_frost <- c("0.833241", "0.7061865", "0.8701754", "0.5990766", "0.5252078", "0.3242844")
 Balrog <- data.frame(Years, Percent_frost)
 View(Balrog)
 
-
+# Plot the data
 ggplot(Balrog, aes(Years, Percent_frost, group = 1)) +
            geom_point() +
            geom_line() +
            labs(x = "Years", y = "Percentage of frost pixels", title = "Variation of the percentage of frost areas from 2000 to 2022")
-
-
-
-
-# Visualize quantile-quantile graphic
-qqnorm(scar, main="Variation of the percentage of frost areas from 2000 to 2022", xlab="Year", ylab="Percentage of Frost Areas", pch=16)
-qqline(scar, datax = FALSE, distribution = qnorm)
-
-# Plot the previous graphics toghether
-par(mfrow=c(2,1))
-plot(scar, main="Variation of the percentage of frost areas from 2000 to 2022", xlab="Year", ylab="Percentage of Frost Areas", pch=16)
-qqnorm(scar, main="Q-Q plot of the variation of the percentage of frost areas from 2000 to 2022", xlab="Year", ylab="Percentage of Frost Areas", pch=16)
-qqline(scar, datax = FALSE, distribution = qnorm)
