a57387802a11eb0a9876b108cbd7e9c0730f6d38
Riccardo Biagini <101262928+Riccard098@users.noreply.github.com>
2022-07-16 16:34:11 +0200

    Update R_code_spctral_indeces.r

--- R_code_spctral_indeces.r
+++ R_code_spctral_indeces.r
@@ -6,14 +6,14 @@ library(rasterdiv)
 # Set workdirecotory
 setwd("C:/Users/kirir/OneDrive/Desktop/lab")
 
-# Import and rename datas
+# Import and rename data
 l1992 <- brick("defor1_.jpg")
 l2006 <- brick("defor2_.jpg")
 
-# Difference Vegetation Index (DVI) of 1992's datas
+# Difference Vegetation Index (DVI) of 1992's data
 dvi1992 = l1992[[1]] - l1992[[2]]
 
-# Calculete Normalized Difference Vegetation Index (NDVI) of 1992's datas
+# Calculete Normalized Difference Vegetation Index (NDVI) of 1992's data
 ndvi1992 = (l1992[[1]] - l1992[[2]]) / (l1992[[1]] + l1992[[2]])
 
 # Create a specific palette
@@ -24,19 +24,19 @@ par(mfrow=c(2,1))
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
 plot(ndvi1992, col=cl)
 
-# Calculete Difference Vegetation Index (DVI) of 2006's datas
+# Calculete Difference Vegetation Index (DVI) of 2006's data
 dvi2006 = l2006[[1]] - l2006[[2]]
 
-# Calculete Normalized Difference Vegetation Index (NDVI) of 2006's datas
+# Calculete Normalized Difference Vegetation Index (NDVI) of 2006's data
 ndvil2006 = (l2006[[1]] - l2006[[2]]) / (l2006[[1]] + l2006[[2]])
 
-# Plot in the same window the NDVI of 1992's datas and the NDVI of 2006's with the specific palette
+# Plot in the same window the NDVI of 1992's data and the NDVI of 2006's with the specific palette
 par(mfrow=c(2,1))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
 
-# Calculate all the Spectral Indecis for the 1992's datas
+# Calculate all the Spectral Indecis for the 1992's data
 si1992 <- spectralIndices(l1992, green=3, red=2, nir=1)
 
-# Plot all the Spectral Indecis for the 1992's datas with the specific palette
+# Plot all the Spectral Indecis for the 1992's data with the specific palette
 plot(si1992, col = cl)
